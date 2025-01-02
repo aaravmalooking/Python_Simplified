aa80d00c37ca85252670f030a430c17f693900c4
Aarav Maloo <aaravmaloov@gmail.com>
2025-01-02 19:31:23 +0530

    Added .first_program.py in the dir, and created a dir called practice for practice

---
+++ first_program.py
@@ -0,0 +1,10 @@
+# So well come to your first program in python.
+# If you might have learned other languages you might know how to print, which means printing something (showing it through the terminal) using stdin (std input)
+# It is really easy to just print something in python, in fact it can be achieved by a single line of code.
+print("Hello, World!") # Remember to always close the string in double quotes.
+# you can even print multiple values using , (I know this does not sound so helpful right now, but later, this is going to be really helpful)
+print("I","am","learning","python")
+# outputs --> I am learning python
+# This is simple, but print function does not just does this, it also takes parameters.
+# for example, you can separate them using the sep= function, which will separate each value by the specified string number anything.
+print("I","am","learning","python", sep=", ") # add a space always or else the output will only use commas, and will not put space between them.
