b8282f54b492a3884a4ee095563d54c43f96db01
Aarav Maloo <aaravmaloov@gmail.com>
2025-01-02 20:17:36 +0530

    updated README.markdown; organised everything for convenience

--- basics/first_program.py
+++ basics/first_program.py
@@ -8,3 +8,10 @@ print("I","am","learning","python")
 # This is simple, but print function does not just does this, it also takes parameters.
 # for example, you can separate them using the sep= function, which will separate each value by the specified string number anything.
 print("I","am","learning","python", sep=", ") # add a space always or else the output will only use commas, and will not put space between them.
+print("I","am","learning","python", end=" okay?\n" )
+# try the above command it will about it in end
+# so basically sep is between each word and end is after the whole thing has been completed
+# you can even combine sep and end.
+print("I","am","learning","python", sep=",", end=" okay?")
+# don't worry about \n, we'll discuss it later
+# now that you have completed print statement, proceed to <vars & esc_chars>
