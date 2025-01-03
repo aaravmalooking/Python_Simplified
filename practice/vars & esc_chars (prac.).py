72ee563caf021468a2797700345065affe95a04d
Aarav Maloo <aaravmaloov@gmail.com>
2025-01-03 20:15:16 +0530

    Created more files, added content onto them, numbers remain

--- practice/vars & esc_chars (prac.).py
+++ practice/vars & esc_chars (prac.).py
@@ -1,4 +1,21 @@
-# so just finished vars & esc_chrs; I think I just put too much info in their
-# hence, you have to practice every day just
+# so just finished vars & esc_chrs; I think I just put too much information in their
+# hence, have to practice every day just
 # so okay now there will be some questions, answer them and then be ready for learning comments.
-# so create a program where you take the user's name and greet him hello and also take his age and say that age.
+# so create a program where take the user's name and greet him hello and also take his age and say that age.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# done, now lets learn comments. navigate to comments.py, for learning comments!...
