15c1b473e396b9e30423a6dab7867a0317424612
Aarav Maloo <aaravmaloov@gmail.com>
2025-01-04 13:53:23 +0530

    finished numbers.py

--- basics/numbers.py
+++ basics/numbers.py
@@ -15,3 +15,18 @@ myfloat = (float(12.567))
 print(type(mycomplex)) # outputs <class 'complex'>
 print(type(myint)) # outputs <class 'int'>
 print(type(myfloat))# outputs <class 'float'>
+
+
+# you can almost perform arithmetic calculations with numbers.
+
+print(12 + 12) # outputs 24
+print(12 - 12) # outputs 0
+print(12 * 12) # outputs 144 # * is used for multiplication
+print(12 / 12) # outputs 1.0 # / is used for division
+print(12 % 12) # outputs 0 # % is used for modulus, which returns the remainder of a division.
+print(12 ** 12) # outputs 1048576 # ** is used for exponentiation
+
+
+# this will be helpful creating a calculator in python, also in machine learning and artificial intelligence.
+# numpy, a module for scientific computing which would again require numbers.
+# numpy_simplified will be also coming soon.
